3b1fb932d1f656cf2fd387e9fefa3feb205f2ef1
jun-s1016 <153084343+jun-s1016@users.noreply.github.com>
2023-12-22 19:42:23 +0900

    23.12.21 함수

--- Program/Solution.c
+++ Program/Solution.c
@@ -1,148 +1,96 @@
 #include <stdio.h>
 
+void Function()
+{  
+	printf("Function");
+}
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int Minus(int x, int y)
+{
+	return x - y;
+}
+
+int Multiple(int x, int y)
+{
+	return x * y;
+}
+
+int Divide(int x, int y)
+{
+	return x / y;
+}
+
+ 
+void Swap(int a, int b)
+{
+	int temp = b;
+	b = a;
+	a = temp;
+	printf("a 변수의 값 : %d\n", a);
+	printf("b 변수의 값 : %d\n", b);
+
+	// 함수를 호출할 때 변수의 값을 인수로 전달하게 되면 함수의 매개 변수에 변수의 복사된 값이 전달되기 때문에
+	// 인수로 전달한 변수는 함수 내에서 일어난 연산에 대해 영향을 받지 않는다.
+}
+
 void main()
 {
-#pragma region 배열
-	// 같은 자료형의 변수들로 이루어진 유한 집합.
-
-
-
-	// 배열의 경우 첫 번째 원소는 0부터 시작합니다.
-
-
-	//array[0] = 100;
-	/*
-	int array[5];
-
-	for (int i = 0; i < 5; i++)
-	{
-		array[i] = i * 100 + 100;
-		printf("array[%d]의 값 : [%d]\n", i, array[i]);
-	}
-
-	// 배열은 원하는 원소에 원하는 값을 저장할 수 있으며,
-	// 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을 가지게 된다.
-
-	int list[] = { 10,20,30,45 };
-
-	int size = sizeof(list) / sizeof(int);
-
-	printf("배열의 사이즈 : %d\n", size);
-	*/
-
-	// 배열의 크기는 생략할 수 있으며
-	// 초기화 목록에서 설장한 요소에 따라 배열의 크기가 결정된다.
-/*
-	int* ptr = NULL;
-
-	int intList[5] = { 1,2,3,4,5 };
-
-	ptr = intList;
-	// 배열의 이름은 배열의 시작주소를 의미합니다.
-
-	printf("ptr변수의 값은 :%p\n", ptr);
-	printf("intList 배열의 시작 주소:%p\n", intList);
-
-	ptr = ptr + 1;
-
-	*ptr = 99;
+#pragma region 함수
 	
-	printf("ptr 변수의 값은 :%p\n", ptr);
-	printf("ptr 변수가 가리키는 값 :%d\n", *ptr);
-	printf("intList 배열[1]의 주소: %p\n", &intList[1]);
-	*/
+	// 하나의 특별한 목적의 작업을 수행하기 위해 독립적으로 설계된 코드의 집합.
+
+	//Function();
+
+	// 함수가 호출될 때 함수가 호출되는 시작 지점으로 넘어가게 된다.
+#pragma region 매개 변수
+
+	// 함수의 정의에서 전달받은 인수를 함수 내부로 전달하기 위해 사용하는 변수.
 	
-	// 배열은 연속적인 메모리 공간을 가지며, 배열의 이름은 배열의 시작주소를 의미한다.
+	/*printf("Add 함수의 결과 : %d\n", Add(10, 20));
+	*/printf("Minus 함수의 결과 : %d\n", Minus(10, 20));
+
+	// 매개 변수는 함수 내부에서만 연산이 이루어지며
+	// 함수가 종료되면 메모리에서 사라지는 특징을 가진다.	
+
+	//printf("Multiple 함수의 결과 : %d\n", Multiple(5.75f, 7.65f));
+	// 함수의 경우 자료형과 반환하는 값의 형태가 일치하지 않으면 원하는 값을 얻을수 었다.
+
+
+#pragma endregion
+
+#pragma region 인수
+
+	// 함수가 호출될 때 매개변수에 실제로 전달되는 값입니다.
+
+	//a(10) ~ b(20) --> a=20 b=10
+
+	int temp = 0;
+	int a = 10;
+	int b = 20;
+
+	temp = b;
+	b = a;
+	a = temp;
+
+	printf("a 변수의 값 : %d\n", a);
+	printf("b 변수의 값 : %d\n", b);
+
+	Swap(a, b);
+	printf("a 변수의 값 : %d\n", a);
+	printf("b 변수의 값 : %d\n", b);
+
+	// 인수의 경우 함수에 있는 매개변수의 수에 따라 전달할 수 있는 인수의 수가 결정되며
+	// 값을 전달하는 인수와 값을 전달받는 매개변수의 자료형이 서로 일치해야 한다.
+#pragma endregion
 
 
 
 #pragma endregion
 
-#pragma region 문자열
-
-	// 연속적인 메모리 공간에 저장된 문자 변수의 집합
-	/*
-	const char* string = "Computer";
-
-	// *string = 'A'; (ERROR) 
-
-	// %s : 문자열을 출력하는 서식 지정자
-	printf("string 변수의 값 : %s\n", string);
-
-	// 문자열의 경우 포인터를 이용하여 문자열 상수를 가리키도록 할 수 있으며,
-	// 문자열 상수는 데이터 영역의 읽기 전용 공간에 저장되기 때문에,
-	// 문자열의 값을 변경할 수 없다.
-
-	string = "Hello";	 
-
-	printf("string 변수의 값 : %s\n", string);
-*/
-	// 문자열은 공백도 함께 매모리 공간에 포함하여 크기가 결정되며,
-	// 마지막에 문자열의 끝을 알려주는 제어 문자(NULL)가 추가됩니다.
-	/*
-	// [A] [L] [I] [S] [T] [A] [R] [\0] [] []
-
-	char name[10] = { "Alistar" };
-
-	printf("name 변수의 값 : %s\n", name);
-
-	name[4] = '\0';
-
-	// 문자열을 저장하게 되면 맨 마지막에 무효의 문자까지 메모리 공간에 저장된다.
-
-	printf("name 변수의 값 : %s\n", name);
-	*/
-	// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만
-	// 문자 배열사이에 무효의 문자를 넣게되면 무효의 문자까지만 문자열을 출력한다.
-
-
-#pragma endregion
-
-#pragma region 최댓값과 최솟값
-	// 01234 에 자료해당하고, int 임시변수에 10 입력. 임시변수의값이 다음
-	// 다음 값과 비교하여 다음 값이 더 클시 변수에 저장. 계속 반복. 
-/*
-int i = 0;
-	int num[5] = { 10,22,35,2,5 };
-	
-	for (int i = 0; i < 5; i++)
-	{
-		if (num[i] < num[i + 1])
-		{
-			printf("%d\n", num[i + 1]);
-		}
-		if (num[i] > num[i + 1])
-		{
-			break;
-		}
-	}
-	*/
-	int dataList[] = { 17,35,42,11,76};
-
-	int max = 0;
-	int min = 0;
-	int arraySize = sizeof(dataList) / sizeof(int); 
-	// 배열의 크기를 물어보는 문장. 배열 자체의 크기 (4X5) = 20 / 자기자료형크기(4) = 5 (배열의크기) 
-
-	max = dataList[0];
-	min = dataList[0];
-
-	for (int i = 0; i < arraySize; i++)
-	{
-		if (max < dataList[i])
-		{
-			max = dataList[i];
-		}
-		if (min > dataList[i])
-		{
-			min = dataList[i];
-		}
-	}
-	printf(" dataList의 최댓값 : %d\n", max); 
-	printf(" dataList의 최솟값 : %d\n", min);
-
-
-
-#pragma endregion
 
 }
---
+++ class 8th/source.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 배열
+	// 같은 자료형의 변수들로 이루어진 유한 집합.
+
+
+
+	// 배열의 경우 첫 번째 원소는 0부터 시작합니다.
+
+
+	//array[0] = 100;
+	/*
+	int array[5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		array[i] = i * 100 + 100;
+		printf("array[%d]의 값 : [%d]\n", i, array[i]);
+	}
+
+	// 배열은 원하는 원소에 원하는 값을 저장할 수 있으며,
+	// 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을 가지게 된다.
+
+	int list[] = { 10,20,30,45 };
+
+	int size = sizeof(list) / sizeof(int);
+
+	printf("배열의 사이즈 : %d\n", size);
+	*/
+
+	// 배열의 크기는 생략할 수 있으며
+	// 초기화 목록에서 설장한 요소에 따라 배열의 크기가 결정된다.
+/*
+	int* ptr = NULL;
+
+	int intList[5] = { 1,2,3,4,5 };
+
+	ptr = intList;
+	// 배열의 이름은 배열의 시작주소를 의미합니다.
+
+	printf("ptr변수의 값은 :%p\n", ptr);
+	printf("intList 배열의 시작 주소:%p\n", intList);
+
+	ptr = ptr + 1;
+
+	*ptr = 99;
+
+	printf("ptr 변수의 값은 :%p\n", ptr);
+	printf("ptr 변수가 가리키는 값 :%d\n", *ptr);
+	printf("intList 배열[1]의 주소: %p\n", &intList[1]);
+	*/
+
+	// 배열은 연속적인 메모리 공간을 가지며, 배열의 이름은 배열의 시작주소를 의미한다.
+
+
+
+#pragma endregion
+
+#pragma region 문자열
+
+	// 연속적인 메모리 공간에 저장된 문자 변수의 집합
+	/*
+	const char* string = "Computer";
+
+	// *string = 'A'; (ERROR)
+
+	// %s : 문자열을 출력하는 서식 지정자
+	printf("string 변수의 값 : %s\n", string);
+
+	// 문자열의 경우 포인터를 이용하여 문자열 상수를 가리키도록 할 수 있으며,
+	// 문자열 상수는 데이터 영역의 읽기 전용 공간에 저장되기 때문에,
+	// 문자열의 값을 변경할 수 없다.
+
+	string = "Hello";
+
+	printf("string 변수의 값 : %s\n", string);
+*/
+// 문자열은 공백도 함께 매모리 공간에 포함하여 크기가 결정되며,
+// 마지막에 문자열의 끝을 알려주는 제어 문자(NULL)가 추가됩니다.
+/*
+// [A] [L] [I] [S] [T] [A] [R] [\0] [] []
+
+char name[10] = { "Alistar" };
+
+printf("name 변수의 값 : %s\n", name);
+
+name[4] = '\0';
+
+// 문자열을 저장하게 되면 맨 마지막에 무효의 문자까지 메모리 공간에 저장된다.
+
+printf("name 변수의 값 : %s\n", name);
+*/
+// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만
+// 문자 배열사이에 무효의 문자를 넣게되면 무효의 문자까지만 문자열을 출력한다.
+
+
+#pragma endregion
+
+#pragma region 최댓값과 최솟값
+	// 01234 에 자료해당하고, int 임시변수에 10 입력. 임시변수의값이 다음
+	// 다음 값과 비교하여 다음 값이 더 클시 변수에 저장. 계속 반복. 
+/*
+int i = 0;
+	int num[5] = { 10,22,35,2,5 };
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (num[i] < num[i + 1])
+		{
+			printf("%d\n", num[i + 1]);
+		}
+		if (num[i] > num[i + 1])
+		{
+			break;
+		}
+	}
+	*/
+
+	//int dataList[] = { 17,35,42,11,76};
+
+	//int max = 0;
+	//int min = 0;
+
+	// 배열의 크기를 물어보는 문장. 배열 자체의 크기 (4X5) = 20 / 자기자료형크기(4) = 5 (배열의크기) 
+
+	//max = dataList[0];
+	//min = dataList[0];
+
+	//int arraySize = sizeof(dataList) / sizeof(int); 
+
+	//for (int i = 0; i < arraySize; i++)
+	//{
+	//	if (max < dataList[i])
+	//	{
+	//		max = dataList[i];
+	//	}
+	//	if (min > dataList[i])
+	//	{
+	//		min = dataList[i];
+	//	}
+	//}
+	//printf(" dataList의 최댓값 : %d\n", max); 
+	//printf(" dataList의 최솟값 : %d\n", min);
+
+
+
+
+#pragma endregion
+
+}
